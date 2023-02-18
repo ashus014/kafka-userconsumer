bfce5692da8cef1b5f6d5dc702d7d03e290672d0
Ashu <ashu.ask.singh@gmail.com>
2023-02-18 23:13:38 +0530

    working with non-primitive data

--- src/main/java/com/ashu/kafka/service/UserConsumerClass.java
+++ src/main/java/com/ashu/kafka/service/UserConsumerClass.java
@@ -1,13 +1,19 @@
 package com.ashu.kafka.service;
 
+import com.ashu.kafka.dto.User;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserConsumerClass {
 
-	@KafkaListener(topics = {"user-topic"})
-    public void consumerUserData(int age){
-        System.out.println("User Age is : " + age);
+//	@KafkaListener(topics = {"user-topic"})
+//    public void consumerUserData(int age){
+//        System.out.println("User Age is : " + age);
+//    }
+
+    @KafkaListener(topics = {"user-topic"})
+    public void consumerUserData(User user){
+        System.out.println("User Age is : " + user.getAge() + " Department : " + user.getDept());
     }
 }
